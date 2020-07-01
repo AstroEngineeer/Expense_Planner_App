3f1700774a893d91f4f909cf51f52051d425869c
Vignesh Ganesan <vicky08rockz@gmail.com>
2020-07-01 13:22:16 +0530

    AFTER DATEFORMATTING

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:Expense_Planner_App/Transcation.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppHome extends StatelessWidget {
-  List transcations = <Transcation>[
+  final List transcations = <Transcation>[
     Transcation(id: "t1", title: "Pizza", amt: 500, date: DateTime.now()),
     Transcation(id: "t2", title: "Buger", amt: 100, date: DateTime.now())
   ];
@@ -40,7 +41,45 @@ class MyAppHome extends StatelessWidget {
           ),
           Column(
               children: transcations.map((tx) {
-            return Card(child: Text(tx.title));
+            return Card(
+                child: Row(
+              children: <Widget>[
+                Container(
+                  child: Text("₹ ${tx.amt}",
+                      style: TextStyle(
+                          color: Colors.purple,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 2),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                        child: Text(
+                      tx.title,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    )),
+                    Container(
+                        child: Text(
+                      DateFormat.yMMMd().format(tx.date),
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold),
+                    )),
+                  ],
+                )
+              ],
+            ));
           }).toList())
         ],
       ),
